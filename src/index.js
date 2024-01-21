1243107f5fdf36b3767ee0dbcc52735c73652352
yarqui <y.pelykh@gmail.com>
2024-01-20 23:33:34 -0500

    fix import issue with concatenation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { concatenation } from "./concatenation";
+import { concatenation } from "./concatenation.js";
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 if (button && input) {
-    button.addEventListener("click", () => {
-        concatenation(input.value, "hello");
-    });
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello");
+  });
 }
